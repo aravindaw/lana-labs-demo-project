612944791da261cff45fe8017582ff35bf813056
Aravinda Weerasekara <aravindaweerasekara@gmail.com>
2019-11-10 22:59:11 +0100

    update test cases to run in docker container

--- src/main/java/com/aravinda/app/testCase/TC_01_LoginTest.java
+++ src/main/java/com/aravinda/app/testCase/TC_01_LoginTest.java
@@ -1,34 +1,35 @@
 package com.aravinda.app.testCase;
 
 import com.aravinda.app.script.LoginScript;
-import org.seleniumx.annotations.DriverSettings;
+import org.openqa.selenium.Platform;
 import org.seleniumx.annotations.Script;
+import org.seleniumx.annotations.ServerSettings;
 import org.seleniumx.util.Set;
 import org.seleniumx.util.TestCase;
 
 public class TC_01_LoginTest extends TestCase {
 
-    //    @ServerSettings(
-//            PLATFORM = Platform.LINUX,
-//            BROWSER = Set.BROWSER.CHROME,
-//            VERSION = "1",
-//            SERVER_PORT = "5555",
-//            SERVER_URL = "http://localhost",
-//            BASE_URL = "http://magellanic.lanalabs.com",
-//            IMPLICIT_WAIT = 100)
-    @DriverSettings(
-            OS = Set.OS.MAC,
-            WINDOW_SIZE = Set.WINDOW_SIZE.DEFAULT,
+    @ServerSettings(
+            PLATFORM = Platform.LINUX,
             BROWSER = Set.BROWSER.CHROME,
+            VERSION = "1",
+            SERVER_PORT = "5555",
+            SERVER_URL = "http://localhost",
             BASE_URL = "http://magellanic.lanalabs.com",
-            IMPLICIT_WAIT = 10000
-    )
+            IMPLICIT_WAIT = 100)
+//    @DriverSettings(
+//            OS = Set.OS.MAC,
+//            WINDOW_SIZE = Set.WINDOW_SIZE.DEFAULT,
+//            BROWSER = Set.BROWSER.CHROME,
+//            BASE_URL = "http://magellanic.lanalabs.com",
+//            IMPLICIT_WAIT = 10000
+//    )
     @Script(script = LoginScript.class)
     public void testCase() {
-        data.put("userName","aravindaweerasekara@gmail.com");
-        data.put("password","test@123");
+        data.put("userName", "aravindaweerasekara@gmail.com");
+        data.put("password", "test@123");
         //assert Model page title
-        data.put("firstPageTitle","LANA - Model");
+        data.put("firstPageTitle", "LANA - Model");
         run(data);
 
     }
--- src/main/java/com/aravinda/app/testCase/TC_02_FilterApplication.java
+++ src/main/java/com/aravinda/app/testCase/TC_02_FilterApplication.java
@@ -2,29 +2,30 @@ package com.aravinda.app.testCase;
 
 import com.aravinda.app.preconditions.LoginPrecondition;
 import com.aravinda.app.script.FilterApplication;
-import org.seleniumx.annotations.DriverSettings;
+import org.openqa.selenium.Platform;
 import org.seleniumx.annotations.Precondition;
 import org.seleniumx.annotations.Script;
+import org.seleniumx.annotations.ServerSettings;
 import org.seleniumx.util.Set;
 import org.seleniumx.util.TestCase;
 
 public class TC_02_FilterApplication extends TestCase {
 
-    //    @ServerSettings(
-//            PLATFORM = Platform.LINUX,
-//            BROWSER = Set.BROWSER.CHROME,
-//            VERSION = "1",
-//            SERVER_PORT = "5555",
-//            SERVER_URL = "http://localhost",
-//            BASE_URL = "http://magellanic.lanalabs.com",
-//            IMPLICIT_WAIT = 100)
-    @DriverSettings(
-            OS = Set.OS.MAC,
-            WINDOW_SIZE = Set.WINDOW_SIZE.DEFAULT,
+    @ServerSettings(
+            PLATFORM = Platform.LINUX,
             BROWSER = Set.BROWSER.CHROME,
+            VERSION = "1",
+            SERVER_PORT = "5555",
+            SERVER_URL = "http://localhost",
             BASE_URL = "http://magellanic.lanalabs.com",
-            IMPLICIT_WAIT = 5000
-    )
+            IMPLICIT_WAIT = 100)
+//    @DriverSettings(
+//            OS = Set.OS.MAC,
+//            WINDOW_SIZE = Set.WINDOW_SIZE.DEFAULT,
+//            BROWSER = Set.BROWSER.CHROME,
+//            BASE_URL = "http://magellanic.lanalabs.com",
+//            IMPLICIT_WAIT = 5000
+//    )
     @Precondition(precondition = LoginPrecondition.class, data = {"aravindaweerasekara@gmail.com", "test@123"})
     @Script(script = FilterApplication.class)
     public void testCase() {
